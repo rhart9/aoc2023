94ccdfc74a98723a8c0dc001797b073ff9da6c0c
russ <hartr.h16@gmail.com>
2024-11-25 23:45:50 -0500

    day 10 pt 2

--- lib/aoc2023/solutions/day10.rb
+++ lib/aoc2023/solutions/day10.rb
@@ -33,9 +33,16 @@ module Aoc2023
     end
 
     def self.solve1
+      return self.solve_common(false)
+    end
+
+    def self.solve_common(save_boundaries)
       val = 0
       orig_row = 0
       orig_col = 0
+      first_s_dir = ""
+
+      @@boundaries = Hash.new { |h, k| h[k] = {} }
 
       @@lines.each_with_index do |line, i|
         line.chars.each_with_index do |char, j|
@@ -46,6 +53,8 @@ module Aoc2023
         end
       end
 
+      next_dir = nil
+
       @@directions.keys.each do |dir|
         @direction = dir
         @row = orig_row
@@ -54,19 +63,32 @@ module Aoc2023
         unless @row == orig_row && @col == orig_col
           next_dir = get_next_dir()
           unless next_dir.nil?
-            @direction = next_dir
+            @direction = first_s_dir = dir
             break
           end
         end
       end
 
       until @row == orig_row && @col == orig_col
+        if save_boundaries
+          add_boundary(@@lines[@row][@col])
+        end
+        @direction = next_dir
         move()
-        @direction = get_next_dir()
+        next_dir = get_next_dir()
         val += 1
       end
 
+      if save_boundaries
+        legend_char = @@legend.select { |k, v| v == "#{first_s_dir}#{@@directions[@direction][:opposite]}" || v == "#{@@directions[@direction][:opposite]}#{first_s_dir}" }.keys[0]
+        add_boundary(legend_char)
+      end
+
       return ((val.to_f + 1) / 2).ceil
+    end
+
+    def self.add_boundary(char)
+      @@boundaries[@row].merge!({ @col => char }) unless char == "-"
     end
 
     def self.move
@@ -99,8 +121,54 @@ module Aoc2023
       return direction
     end
 
+    @@state_changes = {
+      "|" => {
+        :out => :in,
+        :in => :out
+      },
+      "L" => {
+        :out => :edgeLout,
+        :in => :edgeLin
+      },
+      "F" => {
+        :out => :edgeFout,
+        :in => :edgeFin
+      },
+      "7" => {
+        :edgeLin => :out,
+        :edgeLout => :in,
+        :edgeFin => :in,
+        :edgeFout => :out
+      },
+      "J" => {
+        :edgeLin => :in,
+        :edgeLout => :out,
+        :edgeFin => :out,
+        :edgeFout => :in
+      }
+    }
+
     def self.solve2
       val = 0
+
+      self.solve_common(true)
+
+      @@boundaries.keys.each do |row|
+        state = :out
+        in_col = 0
+        
+        @@boundaries[row].keys.sort.each do |col|
+          if state == :in
+            val += (col - in_col - 1)
+          end
+
+          state = @@state_changes[@@boundaries[row][col]][state]
+
+          if state == :in
+            in_col = col
+          end
+        end
+      end
 
       return val
     end
--- spec/aoc2023/day10_spec.rb
+++ spec/aoc2023/day10_spec.rb
@@ -24,11 +24,51 @@ RSpec.describe Aoc2023, class_name: "Aoc2023::Day10" do
     expect(solution).to eq(6_725)
   end
 
-  it "calculates ? for 10 test data pt 2" do
+  it "calculates 1 for 10 test data pt 2" do
     Aoc2023::Day10.read_input("input/day10-test.txt")
 
     solution = Aoc2023::Day10.solve2
   
-    expect(solution).to eq(0)
+    expect(solution).to eq(1)
+  end
+
+  it "calculates 4 for 10 test data pt 2 - tst3" do
+    Aoc2023::Day10.read_input("input/day10-test3.txt")
+
+    solution = Aoc2023::Day10.solve2
+  
+    expect(solution).to eq(4)
+  end
+
+  it "calculates 4 for 10 test data pt 2 - tst4" do
+    Aoc2023::Day10.read_input("input/day10-test4.txt")
+
+    solution = Aoc2023::Day10.solve2
+  
+    expect(solution).to eq(4)
+  end
+
+  it "calculates 8 for 10 test data pt 2 - tst5" do
+    Aoc2023::Day10.read_input("input/day10-test5.txt")
+
+    solution = Aoc2023::Day10.solve2
+  
+    expect(solution).to eq(8)
+  end
+  
+  it "calculates 10 for 10 test data pt 2 - tst6" do
+    Aoc2023::Day10.read_input("input/day10-test6.txt")
+
+    solution = Aoc2023::Day10.solve2
+  
+    expect(solution).to eq(10)
+  end
+
+  it "calculates 383 for 10 data pt 2" do
+    Aoc2023::Day10.read_input("input/day10.txt")
+
+    solution = Aoc2023::Day10.solve2
+  
+    expect(solution).to eq(383)
   end
 end
